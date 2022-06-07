8f7c9fce8dc9883fca685036266592d0964b60d8
quangnguyen12 <nguyenbaoquang1@gmail.com>
2022-06-07 21:58:37 +0700

    Add project files.

---
+++ test6/Controllers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace test6.Controllers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ test6/Controllers/SignInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test6.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
